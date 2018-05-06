ebab96fb63e62b296b4d50ff6a8c904d830a5c40
Gerardo Junior <gerardo.barbosa.da.silva.jr@gmail.com>
2018-05-06 05:01:50 -0300

    Create docker image and compile node

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
-if [ -e "$(pwd)/yarn.lock" ]; then
-    /usr/local/bin/yarn install --pure-lockfile
-    /usr/local/bin/yarn cache clean --force
-elif [ -e "$(pwd)/package.json" ]; then
-    /usr/local/bin/npm install
-    /usr/local/bin/npm cache clean --force
+if [ ! -d "$(pwd)/node_modules" ] || [ -z "$(ls $(pwd)/node_modules)" ]; then
+    if [ -e "$(pwd)/yarn.lock" ]; then
+        /usr/local/bin/yarn install --pure-lockfile
+        /usr/local/bin/yarn cache clean --force
+    elif [ -e "$(pwd)/package.json" ]; then
+        /usr/local/bin/npm install
+        /usr/local/bin/npm cache clean --force
+    fi
 fi
 
-if [ -d "$(pwd)/node_modules" ]; then
-    chgrp -R users "$(pwd)"/node_modules
+if [ ! -d "$(pwd)/.nuxt" ]; then
+    /usr/local/bin/npm run build
 fi
 
-if [ ! -z $@ ];then
-    exec "$@"
+if [ ! -z $@ ]; then
+    if [ ! -z "$(which $1)" ]; then
+        exec "$@"
+    else
+    	/usr/local/bin/npm run "$@"
+    fi
 elif [ -e "$(pwd)/package.json" ]; then
     /usr/local/bin/npm run start
 fi
