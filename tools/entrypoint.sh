f955637b45e4a8ae21bbb5ab89f7ba9129f6d483
Unknown <gerardo.barbosa.da.silva.jr@gmail.com>
2018-05-09 04:36:08 -0300

    Adding commad of script

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -18,8 +18,12 @@ if [ ! -d "$(pwd)/.nuxt" ]; then
     /usr/local/bin/npm run build
 fi
 
-if [ ! -z "$@" ]; then
-    exec "$@" 
+if [[ ! -z "$1" ]]; then
+    if [[ -z "$(which -- $1)" ]]; then
+        /usr/local/bin/npm run "$@"
+    else
+        exec "$@" 
+    fi 
 elif [ -e "$(pwd)/package.json" ]; then
     /usr/local/bin/npm run start
 fi
